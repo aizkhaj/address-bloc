42ceb5dc97211f5b4c1144d1a74d3bab5bb8bf04
Aizaz Khaja <aizazkj@gmail.com>
2017-03-01 20:18:34 -0500

    assign5 fixed and complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,10 +19,10 @@ class AddressBook
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-  def remove_entry(name, phone_number, email) do
+  def remove_entry(name, phone_number, email)
     delete_entry = nil
     entries.each do |entry|
-      if name == entry.name && phone == entry.phone_number && email == entry.email
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
         delete_entry = entry
       end
     end
